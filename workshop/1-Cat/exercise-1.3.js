18729b902f5d67f278a753cd61a1ebedcd0eb2dd
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-03 21:29:41 -0400

    Exercise 1.4

--- workshop/1-Cat/exercise-1.3.js
+++ workshop/1-Cat/exercise-1.3.js
@@ -44,7 +44,6 @@ class Cat {
         this.loneliness -= (minutes*3)
         this.happiness += (minutes);
     }
-
 }
 
 
--- workshop/1-Cat/exercise-1.4.js
+++ workshop/1-Cat/exercise-1.4.js
@@ -10,6 +10,41 @@
 //
 //    It's up to you to decide how quickly these values should change.
 
+class Cat {
+    constructor (initialTiderness, initialHunger, initialLoneliness, initialHappiness){
+        this.tiredness = initialTiderness;
+        this.hunger = initialHunger;
+        this.loneliness = initialLoneliness;
+        this.happiness = initialHappiness;
+    }
+    name = "Boots"
+    species = "Siamese"
+
+    sleep = (hoursSlept) => {
+        this.tiredness -= (hoursSlept*5);
+        this.happiness += (hoursSlept*3);
+    }
+    eat = (kibbles) => {
+        this.hunger -= (kibbles/5)
+        this.happiness += (kibbles);
+    }
+    play = (minutes) => {
+        this.loneliness -= (minutes*3)
+        this.happiness += (minutes);
+    }
+    wait = (minutes) => {
+        this.tiredness += (minutes/5);
+        this.hunger += (minutes/2)
+        this.loneliness += (minutes/2)
+        this.happiness -= (minutes/5);
+    }
+}
+
 // B) Make Boots wait 20 minutes and call then console.log(boots);
 // (the wait function accepts a number that will modify all the cat's properties)
 // DO NOT make a setTimeout that runs after 20 minutes have passed
+
+const boots = new Cat(50, 15, 12, 2)
+console.log(boots)
+boots.wait(20)
+console.log(boots)
